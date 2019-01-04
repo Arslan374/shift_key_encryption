04c0f86ca1cb928500e6a14af664c98b3c2e6ca4
Arslan374 <43539959+Arslan374@users.noreply.github.com>
2019-01-04 21:49:24 +0500

    Add files via upload

---
+++ decrption.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int c,k;
+    printf("c= ");
+    scanf("%d",&c);
+    printf("k= ");
+    scanf("%d",&k);
+    printf("(c-k)%26 = %d\n",(c-k)%26);
+    return 0;
+}
---
+++ encryption.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int p,k;
+    printf("p= ");
+    scanf("%d",&p);
+    printf("k= ");
+    scanf("%d",&k);
+    printf("(p+k)%26 = %d\n",(p+k)%26);
+    return 0;
+}
